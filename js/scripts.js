01b8030c2d27ed382a3f7e26efe6619f858192e5
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-15 05:06:47 -0300

    Implementei a entrada e a manutenção do usuário na sala.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,87 @@
+entrarNaSala();
 const seletorChat = document.querySelector(".chat ul");
-buscarMensagens();
+//buscarMensagens();
 const meuIntervalo = setInterval(buscarMensagens, 3000);
+
+function entrarNaSala(){
+    const usuario = prompt("Qual é o seu nick?");
+    const dados = {name: usuario};
+    const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", dados);
+    requisicao.then(processarRequisicaoEntrarNaSala);
+    requisicao.catch(processarFalhaEntrarNaSala);
+}
+function processarRequisicaoEntrarNaSala(resposta){
+    let conexao = resposta.config.data.replace(/"/g,'');
+    conexao = conexao.replace('{name:', '');
+    conexao = conexao.replace('}', '');
+    manterConexao(conexao);
+}
+function processarFalhaEntrarNaSala(falha){
+    if(falha.response.status === 400){
+        console.log("Erro:400-Falha ao entrar na sala");
+        entrarNaSalaNovaTentativa();
+    } else {
+        console.log("outro-Falha ao entrar na sala");
+    }
+}
+
+function entrarNaSalaNovaTentativa(){
+    const usuario = prompt("Digite outro nick, pois este já esta em uso:");
+    const dados = {name: usuario};
+    const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", dados);
+    requisicao.then(processarRequisicaoEntrarNaSalaNovaTentativa);
+    requisicao.catch(processarFalhaEntrarNaSalaNovaTentativa);
+}
+function processarRequisicaoEntrarNaSalaNovaTentativa(resposta){
+    let conexao = resposta.config.data.replace(/"/g,'');
+    conexao = conexao.replace('{name:', '');
+    conexao = conexao.replace('}', '');
+    manterConexao(conexao);
+}
+function processarFalhaEntrarNaSalaNovaTentativa(falha){
+    if(falha.response.status === 400){
+        console.log("Erro:400-Falha entrar na sala nova tentantiva");
+        entrarNaSalaNovaTentativa();
+    } else {
+        console.log("outro-Falha entrar na sala nova tentantiva");
+    }
+}
+
+function manterConexao(conexao){
+    const dados = {name: conexao};
+    const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", dados);
+    requisicao.then(processarRequisicaoManterConexao);
+    requisicao.catch(processarFalhaManterConexao);
+}
+
+function processarRequisicaoManterConexao(resposta){
+    let conexao = resposta.config.data.replace(/"/g,'');
+    conexao = conexao.replace('{name:', '');
+    conexao = conexao.replace('}', '');
+    setTimeout(manterConexao, 5000, conexao);
+}
+function processarFalhaManterConexao(falha){
+    if(falha.response.status === 400){
+        console.log("Erro:400-Falha na manutenção da conexao");
+    } else {
+        console.log("outro-Falha na manutenção da conexao");
+    }
+}
+
 function buscarMensagens(){
     const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
 
     promessa.then(processarResposta);
     promessa.catch(processarFalha);
-
 }
-
 function processarResposta(resposta){
     exibirMensagens(resposta.data);
 }
+
 function processarFalha(falha){
-    alert(falha);
+    if(falha.response.status === 400){
+        console.log("Erro:400-4");
+    }
 }
 
 function exibirMensagens(listaDeMensagens){
